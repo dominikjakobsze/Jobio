868549fee8bfa414158230191514d15cede82fc5
Dominik Jakobsze <dominikjakobsze00@gmail.com>
2023-11-26 17:07:54 +0100

    model toffer

--- jobio/app/Models/Toffer.php
+++ jobio/app/Models/Toffer.php
@@ -2,10 +2,21 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Toffer extends Model
 {
     use HasFactory;
+    use HasUuids;
+
+    protected $table = 'toffers';
+    protected $primaryKey = 'id';
+    protected $casts = [];
+    protected $guarded = ['id'];
+    public $incrementing = false;
+    protected $keyType = 'string';
+    public $timestamps = true;
+    protected $dateFormat = 'Y-m-d H:i:s';
 }
