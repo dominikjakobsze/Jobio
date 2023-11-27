3c2bbb2f06bc22931a94c3f221dfec5acc286dde
Dominik Jakobsze <dominikjakobsze00@gmail.com>
2023-11-27 14:39:12 +0100

    model tperson

--- jobio/app/Models/Tperson.php
+++ jobio/app/Models/Tperson.php
@@ -2,10 +2,20 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Tperson extends Model
 {
     use HasFactory;
+    use HasUuids;
+
+    protected $table = 'tpeople';
+    protected $primaryKey = 'id';
+    protected $casts = [];
+    protected $guarded = ['id'];
+    public $incrementing = false;
+    protected $keyType = 'string';
+    public $timestamps = true;
 }
