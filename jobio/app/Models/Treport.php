2f27f04c840cae4095572da828a2e1c6170ea32f
Dominik Jakobsze <dominikjakobsze00@gmail.com>
2023-11-27 14:41:34 +0100

    model treport

--- jobio/app/Models/Treport.php
+++ jobio/app/Models/Treport.php
@@ -2,10 +2,20 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Treport extends Model
 {
     use HasFactory;
+    use HasUuids;
+
+    protected $table = 'treports';
+    protected $primaryKey = 'id';
+    protected $casts = [];
+    protected $guarded = ['id'];
+    public $incrementing = false;
+    protected $keyType = 'string';
+    public $timestamps = true;
 }
