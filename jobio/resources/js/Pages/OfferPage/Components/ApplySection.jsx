07217fc3545e74498d693849c4f4d1e3c429321e
Dominik Jakobsze <dominikjakobsze00@gmail.com>
2023-12-20 12:54:18 +0100

    .

---
+++ jobio/resources/js/Pages/OfferPage/Components/ApplySection.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const ApplySection = () => {
+    console.log("ApplySection");
+    return (
+        <div className="bg-transparent p-5 w-full mx-auto f fr fw ss jc is cs mt-10">
+            <div className="flex-[0_1_auto] text-gray-100 bg-sky-600 tracking-wide py-2 px-12 rounded-full text-base font-[700] shadow-lg cup hover:brightness-125">
+                Zaaplikuj
+            </div>
+        </div>
+    );
+};
+
+export default ApplySection;
--- jobio/resources/js/Pages/OfferPage/Main.jsx
+++ jobio/resources/js/Pages/OfferPage/Main.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import TitleSection from "./Components/TitleSection";
 import OptionsSection from "./Components/OptionsSection";
+import ApplySection from "./Components/ApplySection";
 
 const Main = ({ offer }) => {
     console.log("Main");
@@ -11,6 +12,9 @@ const Main = ({ offer }) => {
             }, [])}
             {React.useMemo(() => {
                 return <OptionsSection offer={offer} />;
+            }, [])}
+            {React.useMemo(() => {
+                return <ApplySection />;
             }, [])}
         </>
     );
