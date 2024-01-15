8c53e86d8bda66e37efd44bafe45ae47a07ca7da
Dominik Jakobsze <dominikjakobsze00@gmail.com>
2024-01-15 11:26:56 +0100

    .

--- jobio/resources/js/Pages/Shared/SupportMenu.jsx
+++ jobio/resources/js/Pages/Shared/SupportMenu.jsx
@@ -7,6 +7,8 @@ import { URL as localUrl } from "../../app";
 import ShHeader from "./ShHeader";
 import ShSubHeader from "./ShSubHeader";
 import Spacer from "./Spacer";
+import MenuBlockButtonsContainer from "./MenuBlockButtonsContainer";
+import MenuBlockButton from "./MenuBlockButton";
 
 const SupportMenu = () => {
     return (
@@ -17,43 +19,38 @@ const SupportMenu = () => {
                 W tym panelu otrzymujesz możliwość zarządzania całym systemem i
                 jego opcjami, wprowadzaj zmiany ostrożnie!
             </ShSubHeader>
-            <Spacer type={"small"} />
-            <div className="f fr fw js cs is ss flex-[0_0_100%] bg-transparent p-10 gap-5">
-                <div
-                    onClick={() => {
-                        window.location.href = localUrl + "/test";
-                    }}
-                    className="flex-[0_1_auto] f fr fw jc is ss cs rounded-xl bg-gray-200/50 p-5 hover:brightness-110 cursor-pointer gap-1"
-                >
-                    <LuUsers2 className="text-gray-700 text-5xl font-[700]" />
-                    <h1 className="text-gray-700 text-lg font-[700] flex-[0_0_100%] text-center">
-                        Użytkownicy
-                    </h1>
-                </div>
-                <div className="flex-[0_1_auto] f fr fw jc is ss cs rounded-xl bg-gray-200/50 p-5 hover:brightness-110 cursor-pointer gap-1">
-                    <FaMapLocationDot className="text-gray-700 text-5xl font-[700]" />
-                    <h1 className="text-gray-700 text-lg font-[700] flex-[0_0_100%] text-center">
-                        Mapa Ofert
-                    </h1>
-                </div>
-                <div className="flex-[0_1_auto] f fr fw jc is ss cs rounded-xl bg-gray-200/50 p-5 hover:brightness-110 cursor-pointer gap-1">
-                    <MdLocalOffer className="text-gray-700 text-5xl font-[700]" />
-                    <h1 className="text-gray-700 text-lg font-[700] flex-[0_0_100%] text-center">
-                        Oferty
-                    </h1>
-                </div>
-                <div
-                    onClick={() => {
-                        window.location.href = localUrl + "/options";
-                    }}
-                    className="flex-[0_1_auto] f fr fw jc is ss cs rounded-xl bg-gray-200/50 p-5 hover:brightness-110 cursor-pointer gap-1"
-                >
-                    <IoOptions className="text-gray-700 text-5xl font-[700]" />
-                    <h1 className="text-gray-700 text-lg font-[700] flex-[0_0_100%] text-center">
-                        Filtrowanie
-                    </h1>
-                </div>
-            </div>
+            <Spacer type={"extra-large"} />
+            <Spacer type={"medium"} />
+            <MenuBlockButtonsContainer>
+                <MenuBlockButton
+                    actionTitle={"Mapa Ofert"}
+                    link={`${localUrl}/`}
+                    IconComponent={
+                        <FaMapLocationDot className="text-gray-700 text-5xl font-[700]" />
+                    }
+                />
+                <MenuBlockButton
+                    actionTitle={"Użytkownicy"}
+                    link={`${localUrl}/`}
+                    IconComponent={
+                        <LuUsers2 className="text-gray-700 text-5xl font-[700]" />
+                    }
+                />
+                <MenuBlockButton
+                    actionTitle={"Oferty"}
+                    link={`${localUrl}/`}
+                    IconComponent={
+                        <MdLocalOffer className="text-gray-700 text-5xl font-[700]" />
+                    }
+                />
+                <MenuBlockButton
+                    actionTitle={"Filtrowanie"}
+                    link={`${localUrl}/options`}
+                    IconComponent={
+                        <IoOptions className="text-gray-700 text-5xl font-[700]" />
+                    }
+                />
+            </MenuBlockButtonsContainer>
         </>
     );
 };
