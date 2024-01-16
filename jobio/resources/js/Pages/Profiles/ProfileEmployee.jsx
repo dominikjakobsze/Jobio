d5d73740b1e925cb0b6c9a4b67ce307f44bbafd2
Dominik Jakobsze <dominikjakobsze00@gmail.com>
2024-01-16 22:12:32 +0100

    .

--- jobio/resources/js/Pages/Profiles/ProfileEmployee.jsx
+++ jobio/resources/js/Pages/Profiles/ProfileEmployee.jsx
@@ -2,14 +2,21 @@ import React from "react";
 import MenuHeader from "../Shared/MenuHeader";
 import HomeCrums from "../Shared/HomeCrums";
 import EmployeeMenu from "../Shared/EmployeeMenu";
+import MenuContainerLayout from "../Shared/MenuContainerLayout";
+import Spacer from "../Shared/Spacer";
 
 const ProfileEmployee = () => {
     return (
-        <div className="f fr fw js is cs ss w-full min-h-[100dvh] bg-gray-100">
+        <MenuContainerLayout>
+            <Spacer type={"extra-large"} />
             <MenuHeader />
+            <Spacer type={"extra-small"} />
             <HomeCrums />
+            <Spacer type={"extra-large"} />
             <EmployeeMenu />
-        </div>
+            <Spacer type={"extra-large"} />
+            <Spacer type={"medium"} />
+        </MenuContainerLayout>
     );
 };
 
--- jobio/resources/js/Pages/Shared/EmployeeMenu.jsx
+++ jobio/resources/js/Pages/Shared/EmployeeMenu.jsx
@@ -1,40 +1,42 @@
 import React from "react";
 import { IoDocumentAttach } from "react-icons/io5";
-import { FaMapLocationDot } from "react-icons/fa6";
-import { FaPenClip } from "react-icons/fa6";
+import { MdLocalOffer } from "react-icons/md";
 import { URL as localUrl } from "../../app";
 import ShHeader from "./ShHeader";
+import ShSubHeader from "./ShSubHeader";
+import Spacer from "./Spacer";
+import MenuBlockButtonsContainer from "./MenuBlockButtonsContainer";
+import MenuBlockButton from "./MenuBlockButton";
 
-const EmployeeMenu = () => {
+const EmployerMenu = () => {
     return (
         <>
-            <ShHeader title={"Panel Użytkownika"} />
-            <h6 className="flex-[0_0_65%] text-sm font-[400] text-gray-400 px-10 my-5">
-                W tym panelu otrzymujesz możliwość stworzenia swojego CV i
-                podejrzenia listy ofert na które aplikowałeś
-            </h6>
-            <div className="f fr fw js cs is ss flex-[0_0_100%] bg-transparent p-10 gap-5">
-                <div className="flex-[0_1_auto] f fr fw jc is ss cs rounded-xl bg-gray-200/50 p-5 hover:brightness-110 cursor-pointer gap-1">
-                    <FaMapLocationDot className="text-gray-700 text-5xl font-[700]" />
-                    <h1 className="text-gray-700 text-lg font-[700] flex-[0_0_100%] text-center">
-                        Mapa Ofert
-                    </h1>
-                </div>
-                <div className="flex-[0_1_auto] f fr fw jc is ss cs rounded-xl bg-gray-200/50 p-5 hover:brightness-110 cursor-pointer gap-1">
-                    <IoDocumentAttach className="text-gray-700 text-5xl font-[700]" />
-                    <h1 className="text-gray-700 text-lg font-[700] flex-[0_0_100%] text-center">
-                        Lista Aplikacji
-                    </h1>
-                </div>
-                <div className="flex-[0_1_auto] f fr fw jc is ss cs rounded-xl bg-gray-200/50 p-5 hover:brightness-110 cursor-pointer gap-1">
-                    <FaPenClip className="text-gray-700 text-5xl font-[700]" />
-                    <h1 className="text-gray-700 text-lg font-[700] flex-[0_0_100%] text-center">
-                        Kreator CV
-                    </h1>
-                </div>
-            </div>
+            <ShHeader title={"Panel Pracownika"} />
+            <Spacer type={"small"} />
+            <ShSubHeader>
+                W tym panelu otrzymujesz możliwość przejrzenia złożonych CV,
+                stworzenia swojego CV itp.
+            </ShSubHeader>
+            <Spacer type={"extra-large"} />
+            <Spacer type={"medium"} />
+            <MenuBlockButtonsContainer>
+                <MenuBlockButton
+                    actionTitle={"Lista Aplikacji"}
+                    link={`${localUrl}/`}
+                    IconComponent={
+                        <MdLocalOffer className="text-gray-700 text-5xl font-[700]" />
+                    }
+                />
+                <MenuBlockButton
+                    actionTitle={"Kreator CV"}
+                    link={`${localUrl}/`}
+                    IconComponent={
+                        <IoDocumentAttach className="text-gray-700 text-5xl font-[700]" />
+                    }
+                />
+            </MenuBlockButtonsContainer>
         </>
     );
 };
 
-export default EmployeeMenu;
+export default EmployerMenu;
